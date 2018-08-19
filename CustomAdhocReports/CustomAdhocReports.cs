ee2b9e267b5b513206f3df532c071219b12cc2cf
Michael Hornaday <michael.hornaday@outlook.com>
2018-08-18 23:16:29 -0400

    Implemented Custom Attribute Code in CustomReportDefinition.cs

---
+++ CustomAdhocReports/CustomAdhocReports.cs
@@ -0,0 +1,32 @@
+﻿using Izenda.BI.Framework.CustomConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Izenda.BI.Framework.Models;
+using System.ComponentModel.Composition;
+using Izenda.BI.Framework.Models.ReportDesigner;
+using Izenda.BI.Framework.Utility;
+using Izenda.BI.Framework.Models.Contexts; //For Referencing User Context
+
+namespace CustomAdhocReports
+{
+    [Export(typeof(IAdHocExtension))]
+    class CustomAdhocReports : DefaultAdHocExtension
+    {
+        /// <summary>
+        /// Adds custom formats for a specified data type.
+        /// </summary>
+        public override List<DataFormat> LoadCustomDataFormat()
+        {
+            return CustomDataFormat.LoadCustomDataFormat();
+        }
+
+        /// <summary>
+        /// Customizes the report content on the fly before it is executed.
+        /// </summary>
+        public override ReportDefinition OnPreExecute(ReportDefinition reportDefinition)
+        {
+            return CustomReportDefinition.OnPreExecute(reportDefinition);
+        }
+    }
+}
---
+++ CustomAdhocReports/CustomReportDefinition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Izenda.BI.Framework.Models.ReportDesigner;
+using Izenda.BI.Framework.Constants;
+using System.Collections.Generic;
+
+namespace CustomAdhocReports
+{
+    /// <summary>
+    /// The custom report definition class.
+    /// </summary>
+    static class CustomReportDefinition
+    {
+        /// <summary>
+        /// Customizes the report definition before it is executed.
+        /// </summary>
+        /// <param name="reportDefinition">The report definition to be customized.</param>
+        /// <returns>Returns the customized report definition.</returns>
+        public static ReportDefinition OnPreExecute(ReportDefinition reportDefinition)
+        {
+
+            // Updates a column's name/alias
+            const string columnToBeCustomized = "Status";
+            foreach (var reportPart in reportDefinition.ReportPart.Select(f => f.ReportPartContent))
+            {
+                if (reportPart.GetAllFields().Any(f => f.FieldNameAlias == columnToBeCustomized))
+                {
+                    var filteredFieldsWithColumn = reportPart.GetAllFields().Where(f => f.FieldNameAlias == columnToBeCustomized).ToList();
+                    filteredFieldsWithColumn.ForEach(f => f.FieldNameAlias = "OrderStatus");
+                }
+            }
+
+            //NewRelic Custom Parameter
+            var ReportName = reportDefinition.Name;
+            var eventAttributes = new Dictionary<string, object>() { { "ReportDefinition", ReportName } };
+            NewRelic.Api.Agent.NewRelic.RecordCustomEvent("DockerApiCustomEvent", eventAttributes);
+            return reportDefinition;
+        }
+    }
+}
